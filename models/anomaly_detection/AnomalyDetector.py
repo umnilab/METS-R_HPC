24a41fb7e51e3d176e644f3d3f56c82ee8c3c800
ZengxiangLei <lei67@purdue.edu>
2024-07-20 15:13:45 -0400

    Added general anomaly detectors

---
+++ models/anomaly_detection/AnomalyDetector.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from adtk.detector import ThresholdAD, QuantileAD, InterQuartileRangeAD, GeneralizedESDTestAD, PersistAD, LevelShiftAD, VolatilityShiftAD, SeasonalAD, AutoregressionAD, MinClusterDetector, OutlierDetector, RegressionAD, PcaAD
+from sklearn.cluster import KMeans
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.linear_model import LinearRegression
+
+class AnomalyDetector():
+    def __init__(self, data_scheme, size=10000):
+        # initialize the data frame according to the scheme, which is a dictionary with column names and types
+        self.data_scheme = data_scheme
+        self.size = size
+        
+        # create an empty DataFrame with specified columns and data types, there must be a column named "timestamp" with datetime64 type
+        if "timestamp" not in data_scheme:
+            raise ValueError("The data scheme must contain a column named 'timestamp' with datetime64 type")
+        
+        self.historical_data = pd.DataFrame(columns=data_scheme.keys())
+        for col, dtype in data_scheme.items():
+            self.historical_data[col] = self.historical_data[col].astype(dtype)
+
+        # use the timestamp as the index
+        self.historical_data.set_index("timestamp", inplace=True)
+
+    def detect_anomaly(self, new_data):
+        # add new_data to the historical data
+        self.add_history_data(new_data)
+
+        # go through the data and detect anomalies
+        anomaly_results = {}
+
+        # TODO: finish and test ech anomaly detection function
+        for method in [ThresholdAD, QuantileAD, InterQuartileRangeAD, GeneralizedESDTestAD, PersistAD, LevelShiftAD, VolatilityShiftAD, SeasonalAD, AutoregressionAD, MinClusterDetector, OutlierDetector, RegressionAD, PcaAD]:
+            detector = method()
+            result = detector.fit_detect(self.historical_data)
+            
+            # trim the anomaly output to the size of the new_data
+            result_trimmed = result.iloc[-len(new_data):] if len(result) > len(new_data) else result
+            
+            anomaly_results[method.__name__] = result_trimmed
+
+        return anomaly_results
+
+    def format_data(self, data):
+        # format the data to be added to the historical data with correct data types
+        formatted_data = pd.DataFrame(data, columns=self.data_scheme.keys())
+        for col, dtype in self.data_scheme.items():
+            formatted_data[col] = formatted_data[col].astype(dtype)
+        formatted_data.set_index("timestamp", inplace=True)
+        return formatted_data
+
+    def add_history_data(self, data):
+        # add the data to the historical data
+        self.historical_data = pd.concat([self.historical_data, self.format_data(data)])
+        
+        # sort the data by the index
+        self.historical_data = self.historical_data.sort_values(by="timestamp")
+        
+        # cut the data to the size
+        if len(self.historical_data) > self.size:
+            self.historical_data = self.historical_data.iloc[-self.size:]
+
+      
