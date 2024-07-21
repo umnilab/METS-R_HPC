f6fd6174107bba34af09d7c8b30b4df4bf52637d
RuichenTan <143303491+RuichenTan@users.noreply.github.com>
2024-07-20 19:14:43 -0500

    Update AnomalyDetector.py

--- models/anomaly_detection/AnomalyDetector.py
+++ models/anomaly_detection/AnomalyDetector.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from adtk.detector import ThresholdAD, QuantileAD, InterQuartileRangeAD, GeneralizedESDTestAD, PersistAD, LevelShiftAD, VolatilityShiftAD, SeasonalAD, AutoregressionAD, MinClusterDetector, OutlierDetector, RegressionAD, PcaAD
+from adtk.visualization import plot
 from sklearn.cluster import KMeans
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.linear_model import LinearRegression
@@ -28,14 +29,31 @@ class AnomalyDetector():
         # go through the data and detect anomalies
         anomaly_results = {}
 
+        methods_with_params = [
+            (ThresholdAD, {"high": 1, "low": 0}),
+            (QuantileAD, {"high": 0.99, "low": 0.01}),
+            (InterQuartileRangeAD, {"c": 1.5}),
+            (GeneralizedESDTestAD, {"alpha": 0.3}),
+            (LevelShiftAD, {"c": 6.0, "side": 'both', "window": 5}),
+            (VolatilityShiftAD, {"c": 6.0, "side": 'positive', "window": 30}),
+            (SeasonalAD, {"c": 3.0, "side": "both"}),
+            (AutoregressionAD, {"n_steps": 7*2, "step_size": 24, "c": 3.0}),
+            (MinClusterDetector, {"detector": KMeans(n_clusters=3)}),
+            (OutlierDetector, {"detector": LocalOutlierFactor(contamination=0.05)}),
+            (RegressionAD, {"regressor": LinearRegression(), "target": "Speed (kRPM)", "c": 3.0}),
+            (PcaAD, {"k": 1}),
+            ]
+ 
         # TODO: finish and test ech anomaly detection function
-        for method in [ThresholdAD, QuantileAD, InterQuartileRangeAD, GeneralizedESDTestAD, PersistAD, LevelShiftAD, VolatilityShiftAD, SeasonalAD, AutoregressionAD, MinClusterDetector, OutlierDetector, RegressionAD, PcaAD]:
-            detector = method()
-            result = detector.fit_detect(self.historical_data)
-            
+        for method, params in methods_with_params:
+            detector = method(**params)
+            if method == ThresholdAD:
+                result = detector.detect(self.historical_data)
+            else: 
+                result = detector.fit_detect(self.historical_data)
+                
             # trim the anomaly output to the size of the new_data
             result_trimmed = result.iloc[-len(new_data):] if len(result) > len(new_data) else result
-            
             anomaly_results[method.__name__] = result_trimmed
 
         return anomaly_results
